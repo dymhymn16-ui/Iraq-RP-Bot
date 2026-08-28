f629774a9444012e1370d8902802846991a5f89a
dymhymn16-ui <dymhymn16@gmail.com>
2026-08-28 21:04:36 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    roles: {
+        owner: "1541813983089008741",
+
+        policeLeader: "1541482535454777456",
+        policeDeputy: "1541482645009989642",
+        policeOfficer: "1541482824492650576",
+
+        gangLeader: "1542530373085565099",
+        gangDeputy: "1541517880703713280",
+        gangMember: "1542530443444879470",
+
+        prisoner: "1533023062587342940"
+    },
+
+    currencyName: "دولار RP 💵"
+};
